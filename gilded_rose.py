af81aa2d9b3d64ff4f3c277286ccf2ff02f90af1
ruchenlu <ru.ch@northeastern.edu>
2024-10-08 21:02:00 -0700

    Refactored GildedRose with Strategy pattern and fixed all tests

--- gilded_rose.py
+++ gilded_rose.py
@@ -11,6 +11,51 @@ class Item:
     def __repr__(self):
         return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
 
+from abc import ABC, abstractmethod
+class ItemStrategy(ABC):
+    @abstractmethod
+    def update(self, item):
+        pass
+
+class RegularItemStrategy(ItemStrategy):
+    def update(self, item):
+        item.sell_in -= 1
+        if item.quality > 0:
+            item.quality -= 1
+        if item.sell_in < 0 and item.quality > 0:
+            item.quality -= 1
+
+
+class AgedBrieStrategy(ItemStrategy):
+    def update(self, item):
+        item.sell_in -= 1
+        if item.quality < 50:
+            item.quality += 1
+        if item.sell_in < 0 and item.quality < 50:
+            item.quality += 1
+
+
+class BackstagePassStrategy(ItemStrategy):
+    def update(self, item):
+        item.sell_in -= 1
+        if item.sell_in < 0:
+            item.quality = 0
+        elif item.sell_in < 5:
+            item.quality = min(item.quality + 3, 50)
+        elif item.sell_in < 10:
+            item.quality = min(item.quality + 2, 50)
+        else:
+            item.quality += 1
+
+
+class ConjuredItemStrategy(ItemStrategy):
+    def update(self, item):
+        item.sell_in -= 1
+        if item.quality > 0:
+            item.quality = max(0, item.quality - 2)
+        if item.sell_in < 0 and item.quality > 0:
+            item.quality = max(0, item.quality - 2)
+
 
 class GildedRose(object):
 
@@ -20,30 +65,45 @@ class GildedRose(object):
 
     def update_quality(self):
         for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
-            else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in = item.sell_in - 1
-            if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
-                    else:
-                        item.quality = item.quality - item.quality
-                else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
+            strategy = self.get_strategy(item)
+            strategy.update(item)
+
+    def get_strategy(self, item):
+        if item.name == "Aged Brie":
+            return AgedBrieStrategy()
+        elif item.name == "Backstage passes to a TAFKAL80ETC concert":
+            return BackstagePassStrategy()
+        elif item.name.startswith("Conjured"):
+            return ConjuredItemStrategy()
+        else:
+            return RegularItemStrategy()
+
+#     def update_quality(self):
+#         for item in self.items:
+#             if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
+#                 if item.quality > 0:
+#                     if item.name != "Sulfuras, Hand of Ragnaros":
+#                         item.quality = item.quality - 1
+#             else:
+#                 if item.quality < 50:
+#                     item.quality = item.quality + 1
+#                     if item.name == "Backstage passes to a TAFKAL80ETC concert":
+#                         if item.sell_in < 11:
+#                             if item.quality < 50:
+#                                 item.quality = item.quality + 1
+#                         if item.sell_in < 6:
+#                             if item.quality < 50:
+#                                 item.quality = item.quality + 1
+#             if item.name != "Sulfuras, Hand of Ragnaros":
+#                 item.sell_in = item.sell_in - 1
+#             if item.sell_in < 0:
+#                 if item.name != "Aged Brie":
+#                     if item.name != "Backstage passes to a TAFKAL80ETC concert":
+#                         if item.quality > 0:
+#                             if item.name != "Sulfuras, Hand of Ragnaros":
+#                                 item.quality = item.quality - 1
+#                     else:
+#                         item.quality = item.quality - item.quality
+#                 else:
+#                     if item.quality < 50:
+#                         item.quality = item.quality + 1
--- test_gilded_rose.py
+++ test_gilded_rose.py
@@ -5,12 +5,96 @@ from gilded_rose import Item, GildedRose
 
 
 class GildedRoseTest(unittest.TestCase):
-    def test_foo(self):
-        items = [Item("foo", 0, 0)]
+    # def test_foo(self):
+    #     items = [Item("foo", 0, 0)]
+    #     gilded_rose = GildedRose(items)
+    #     gilded_rose.update_quality()
+    #     self.assertEquals("fixme", items[0].name)
+
+<<<<<<< Updated upstream
+=======
+    # Test +5 Dexterity Vest decreases in quality and sell_in over time
+    def test_vest_item_should_decrease_after_one_day(self):
+        vest = "+5 Dexterity Vest"
+        items = [
+            Item(vest, 1, 2),
+            Item(vest, 9, 19),
+            Item(vest, 4, 6)
+        ]
+        gr = GildedRose(items)
+        gr.update_quality()
+
+        # self.assertEqual(items[0].sell_in, 1)  # Deliberately incorrect: should be 0
+        # self.assertEqual(items[0].quality, 2)  # Deliberately incorrect: should be 1
+        #
+        # self.assertEqual(items[1].sell_in, 10)  # Deliberately incorrect: should be 8
+        # self.assertEqual(items[1].quality, 20)  # Deliberately incorrect: should be 18
+        #
+        # self.assertEqual(items[2].sell_in, 5)  # Deliberately incorrect: should be 3
+        # self.assertEqual(items[2].quality, 7)  # Deliberately incorrect: should be 5
+        self.assertEqual(items[0].sell_in, 0)
+        self.assertEqual(items[0].quality, 1)
+
+        self.assertEqual(items[1].sell_in, 8)
+        self.assertEqual(items[1].quality, 18)
+
+        self.assertEqual(items[2].sell_in, 3)
+        self.assertEqual(items[2].quality, 5)
+
+    # Test Aged Brie increases in quality over time
+    def test_aged_brie_increases_in_quality(self):
+        items = [
+            Item("Aged Brie", 2, 0),  # Normal case
+            Item("Aged Brie", 0, 10),  # After sell-by date has passed
+            Item("Aged Brie", 5, 50)  # Quality at maximum value
+        ]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        self.assertEquals("fixme", items[0].name)
 
+        # Deliberately set wrong expected values to ensure the test fails
+        # self.assertEqual(items[0].quality, 2)  # Deliberately incorrect: expected value should be 1
+        # self.assertEqual(items[1].quality, 12)  # Deliberately incorrect: expected value should be 12
+        # self.assertEqual(items[2].quality, 49)  # Deliberately incorrect: quality should remain 50
+        self.assertEqual(items[0].quality, 1)
+        self.assertEqual(items[1].quality, 12)
+        self.assertEqual(items[2].quality, 50)
+
+    # Test Backstage passes increase in quality as the event approaches
+    def test_backstage_passes_increases_in_quality(self):
+        items = [
+            Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),  # 15 days left
+            Item("Backstage passes to a TAFKAL80ETC concert", 5, 30),  # 5 days left
+            Item("Backstage passes to a TAFKAL80ETC concert", 1, 45)  # 1 day left
+        ]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+
+        # # Deliberately set wrong expected values to ensure the test fails
+        # self.assertEqual(items[0].quality, 22)  # Deliberately incorrect: expected value should be 21
+        # self.assertEqual(items[1].quality, 35)  # Deliberately incorrect: expected value should be 33
+        # self.assertEqual(items[2].quality, 50)  # Deliberately incorrect: expected value should be 48
+        self.assertEqual(items[0].quality, 21)
+        self.assertEqual(items[1].quality, 33)
+        self.assertEqual(items[2].quality, 48)
+
+    # Test Conjured Mana Cake quality decreases faster
+    def test_conjured_mana_cake_decreases_in_quality(self):
+        items = [
+            Item("Conjured Mana Cake", 3, 6),  # 3 days left
+            Item("Conjured Mana Cake", 0, 6),  # After sell-by date has passed
+            Item("Conjured Mana Cake", 1, 1)  # Quality close to 0
+        ]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+
+        # # Deliberately set wrong expected values to ensure the test fails
+        # self.assertEqual(items[0].quality, 5)  # Deliberately incorrect: expected value should be 4
+        # self.assertEqual(items[1].quality, 3)  # Deliberately incorrect: expected value should be 2
+        # self.assertEqual(items[2].quality, -1)  # Deliberately incorrect: quality should not be negative, expected value should be 0
+        self.assertEqual(items[0].quality, 4)
+        self.assertEqual(items[1].quality, 2)
+        self.assertEqual(items[2].quality, 0)
+>>>>>>> Stashed changes
 
 if __name__ == '__main__':
     unittest.main()
